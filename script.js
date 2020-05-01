cc93508b6f59a265b0855afcb6676fb346a3634c
James Coen <jcoen44@hotmail.com>
2020-05-01 15:26:51 +0100

    updated to include UI

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// DOM elements
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const playerScoreDisplay = document.getElementById('player-score');
+const compScoreDisplay = document.getElementById('comp-score');
+const playAgainBtn = document.getElementById('play-again');
+const roundText = document.getElementById('round-text');
+const endOfGameText = document.getElementById('end-game-text');
+const overModal = document.getElementById('overModal');
+
 // Global variables
 let gamesToPlay = 5;
 let playerScore = 0;
@@ -14,36 +25,59 @@ function computerPlay() {
 
 // Play function
 function play(playerSelection, computerSelection) {
+
+    // Game conditions
     if(playerSelection === 'rock' && computerSelection === 'Paper') {
         compScore++;
-        return "You Lose, Paper beats Rock";
+        compScoreDisplay.innerText = compScore;
+        roundText.innerText = "You Lose, Paper beats Rock";
     } else if(playerSelection === 'scissors' && computerSelection === 'Rock') {
         compScore++
-        return "You Lose, Rock beats Scissors";
+        compScoreDisplay.innerText = compScore;
+        roundText.innerText = "You Lose, Rock beats Scissors";
     } else if(playerSelection === 'paper' && computerSelection === 'Scissors') {
         compScore++
-        return "You Lose, Scissors beats Paper";
+        compScoreDisplay.innerText = compScore;
+        roundText.innerText = "You Lose, Scissors beats Paper";
     } else if(playerSelection === computerSelection.toLowerCase()) {
         gamesToPlay++
-        return 'This round is a draw';
+        roundText.innerText = 'This round is a draw';
     } else {
         playerScore++
-        return `You Win, ${playerSelection} beats ${computerSelection}`;
+        playerScoreDisplay.innerText = playerScore;
+        roundText.innerText = `You Win, ${playerSelection} beats ${computerSelection}`;
     }
+
+    // Call check score function
+    checkScore();
 }
 
 
-function game() {
-    // Loop to call play function 5 times
-    for(let i = 0; i < gamesToPlay; i++) {
-        const playerSelection = prompt('Enter Rock, Paper or Scissors').toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(play(playerSelection, computerSelection));
+// Check when score is 5 so game is over
+function checkScore() {
+    if(playerScore === 5 || compScore === 5) {
+        gameOver();
     }
+}
 
+function gameOver() {
+    roundText.innerText = '';
+    overModal.classList.add('show');
     // Output the winner best out of 5 games
-    return compScore > playerScore ? `Computer Wins! ${compScore} games to ${playerScore}` : `Player Wins! ${playerScore} games to ${compScore}`;
+    endOfGameText.innerText = compScore > playerScore ? `Computer Wins! ${compScore} games to ${playerScore}` : `Player Wins! ${playerScore} games to ${compScore}`;
 }
 
-// Init game function
-console.log(game());
+function resetGame() {
+    // Reset scores and remove modal
+    compScoreDisplay.innerText = 0;
+    playerScoreDisplay.innerText = 0;
+    playerScore = 0;
+    compScore = 0;
+    overModal.classList.remove('show');
+}
+
+// Event listeners
+rock.addEventListener('click', () => play('rock', computerPlay()));
+paper.addEventListener('click', () => play('paper', computerPlay()));
+scissors.addEventListener('click', () => play('scissors', computerPlay()));
+playAgainBtn.addEventListener('click', resetGame);
